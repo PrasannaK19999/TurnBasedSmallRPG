2067e3a6dc22f9c8ccec186bc0c7f5d9c5f429d8
Prasanna <gokutheri@gmail.com>
2024-10-08 17:32:24 +0530

    Seperate branch for Gameplay Testing

--- TurnBasedSmallRPG.cpp
+++ TurnBasedSmallRPG.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 #include "Enemy/Enemy.h"
 #include "Player/Player.h"
@@ -6,14 +7,20 @@
 
 int main()
 {
-	int total_Players = 0;
 	std::cout << "Let's  the Game Begin... \n" << std::endl;
 
 	Enemy Goblin;
+	std::vector<Player*> total_players;
 	
 	std::cout << " How Many Players in our Game.. : " << std::endl;
-	std::cin >> total_Players;
+	int player_count = 0;
+	std::cin >> player_count;
 
+	for (int i = 0; i < player_count; i++)
+	{
+		total_players.push_back(new Player());
+	}
 
+	std::cout << total_players.size();
 
 }
